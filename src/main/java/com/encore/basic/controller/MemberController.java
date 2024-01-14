8ae9eb60b1b51a6af54aa270893852fd121a4780
unknown <1wkdwnsgur@gmail.com>
2024-01-14 23:36:16 +0900

    coding test

--- src/main/java/com/encore/basic/controller/MemberController.java
+++ src/main/java/com/encore/basic/controller/MemberController.java
@@ -1,12 +1,33 @@
 package com.encore.basic.controller;
 
+import com.encore.basic.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
     @GetMapping("members")
     public String members(){
-        return "member-list";
+        return "/member/member-list";
+    }
+
+    @GetMapping("/member/create-screen")
+    public String createScreen(){
+        return "/member/member-create";
+    }
+
+    @PostMapping("/member/create")
+    @ResponseBody
+    public String memberCreate(@RequestBody Member member){
+        System.out.println(member);
+        return "member";
+    }
+
+    @GetMapping("/member/create-clear")
+    public String createClear(){
+        return "create";
     }
 }
--- src/main/java/com/encore/basic/domain/Member.java
+++ src/main/java/com/encore/basic/domain/Member.java
@@ -1,4 +1,10 @@
 package com.encore.basic.domain;
 
+import lombok.Data;
+
+@Data
 public class Member {
+    private String name;
+    private String email;
+    private String password;
 }
